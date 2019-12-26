683f5ea06d3f8d362df148713c6dae3ca57352e8
shesonglin <shesonglin@bytedance.com>
2019-12-26 17:43:11 +0800

    lab3 basic struct

---
+++ lab3/include/Aes.h
@@ -0,0 +1,14 @@
+//
+// Created by 佘崧林 on 2019/12/26.
+//
+
+#ifndef LAB3_AES_H
+#define LAB3_AES_H
+
+
+class Aes {
+
+};
+
+
+#endif //LAB3_AES_H
---
+++ lab3/src/Aes.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 佘崧林 on 2019/12/26.
+//
+
+#include "../include/Aes.h"
+#include "AesConst.h"
